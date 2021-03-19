b8d56570de2b014fe5ede94eca7f884b363b649a
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-03-19 11:08:53 +0200

    reverted to original cords middleware config

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 export function newApp(): Express {
   const app = express();
 
-  app.use(cors({ allowedHeaders: ["Content-Type", "Authorization"] }));
+  app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
