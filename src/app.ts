a81d984fcf4fc61a92f56f926e4073b69c753a66
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-03-19 10:54:59 +0200

    changed cors origin setting

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,11 @@ import cors from "cors";
 export function newApp(): Express {
   const app = express();
 
-  app.use(cors());
+  app.use(
+    cors({
+      origin: "https://community-song.herokuapp.com/",
+    })
+  );
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
