365cfed3bc01e17a48a62760dff4fc4649c270e2
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-03-23 11:42:47 +0200

    used songRouter as middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response, Express } from "express";
 import cors from "cors";
-import fileUpload from "express-fileupload";
-import morgan from "morgan";
+import songRouter from "./routes/songRouter";
 
 export function newApp(): Express {
   const app = express();
@@ -9,6 +8,8 @@ export function newApp(): Express {
   app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+
+  app.use("/song", songRouter);
 
   app.get("/", (_, res: Response) => {
     res.send("Hello from the backend!");
