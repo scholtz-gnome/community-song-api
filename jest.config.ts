c373a56ab7c6ab3f6803c8ce0b5f3cc715663e36
jcqvisser <jcqvisser@gmail.com>
2021-03-26 10:23:38 +0200

    Switch back to ts-jest for testing and configure launch.json

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,6 @@
 module.exports = {
   testEnvironment: "node",
-  testRegex: "(/dist/__tests__/.*|(\\.|/)(test|spec))\\.[j]s?$",
+  preset: "ts-jest",
+  testRegex: "(/dist/__tests__/.*|(\\.|/)(test|spec))\\.[t]s?$",
   verbose: true,
 };
