d5d8a8f9578140ad7c2a0eb6ffa325d20bdbd5f6
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-03-26 16:12:12 +0200

    Remove file size validation

--- src/controllers/songController.ts
+++ src/controllers/songController.ts
@@ -3,7 +3,6 @@ import { UploadedFile } from "express-fileupload";
 
 export const getSongs = (req: Request, res: Response) => {
   // query db for all songs
-  // console.log(db);
 };
 
 export const postSong = (req: Request, res: Response) => {
@@ -15,30 +14,22 @@ export const postSong = (req: Request, res: Response) => {
 
   const song = req.files.file as UploadedFile;
 
-  if (song.size <= 10) { // TODO: don't filter for file size here - use middleware config
-    song.mv(
-      `/Users/stephenscholtz/projects/community-song-api/uploads/${song.name}`,
-      (err) => {
-        if (err) {
-          console.error(err);
-          return res.status(400).json({
-            success: false,
-            message: "Your file upload was unsuccesful.",
-          });
-        }
-
-        res.json({
-          success: true,
-          message: "Your file was uploaded successfully.",
-          fileName: song.name,
-          filePath: `/uploads/${song.name}`,
+  song.mv(
+    `/Users/stephenscholtz/projects/community-song-api/uploads/${song.name}`,
+    (err) => {
+      if (err) {
+        return res.status(400).json({
+          success: false,
+          message: "Your file upload was unsuccesful.",
         });
       }
-    );
-  } else {
-    res.status(400).json({
-      success: false,
-      message: "File size of 5MB is too big. Can't exceed 4MB.",
-    });
-  }
+
+      res.status(200).json({
+        success: true,
+        message: "Your file was uploaded successfully.",
+        fileName: song.name,
+        filePath: `/uploads/${song.name}`,
+      });
+    }
+  );
 };
