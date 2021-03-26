b691328fe4617484be03b04a4f0c139f4da368b9
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-03-26 16:11:01 +0200

    Add max size to config and configure fileupload validation

--- src/routes/songRouter.ts
+++ src/routes/songRouter.ts
@@ -1,13 +1,18 @@
 import { Router } from "express";
 import fileUpload from "express-fileupload";
 import { getSongs, postSong } from "../controllers/songController";
-
-const FIFTY_MEGABYTES = 50 * 1024 * 1024; // TODO extract to config
+import { config } from "../../config";
 
 const songRouter: Router = Router();
 
-songRouter.get("/songs", getSongs);
-songRouter.use(fileUpload({ limits: { fileSize: FIFTY_MEGABYTES } }));
-songRouter.post("/songs", postSong);
+songRouter.get("/", getSongs);
+songRouter.use(
+  fileUpload({
+    limits: { fileSize: config.MAX_FILE_SIZE },
+    abortOnLimit: true,
+    responseOnLimit: "File is too big. Max size is 50MB.",
+  })
+);
+songRouter.post("/", postSong);
 
 export default songRouter;
