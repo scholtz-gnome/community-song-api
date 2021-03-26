d2a7d261c56cdc1b6553ebeabe93f90ef71b3206
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-03-26 16:13:58 +0200

    Add MAX_FILE_SIZE constant to config

--- config.ts
+++ config.ts
@@ -3,9 +3,11 @@ import "dotenv/config";
 interface Config {
   PORT: string | undefined;
   NODE_ENV: string;
+  MAX_FILE_SIZE: number;
 }
 
 export const config: Config = {
   PORT: process.env.PORT,
   NODE_ENV: process.env.NODE_ENV || "test",
+  MAX_FILE_SIZE: 50 * 1024 * 1024,
 };
