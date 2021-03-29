24d895f51ada62db07d5f47ac76559f8172f98b8
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-03-29 17:44:56 +0200

    Remove express fileUpload config and add debug for upload process

--- src/routes/songRouter.ts
+++ src/routes/songRouter.ts
@@ -1,18 +1,11 @@
 import { Router } from "express";
 import fileUpload from "express-fileupload";
 import { getSongs, postSong } from "../controllers/songController";
-import { config } from "../../config";
 
 const songRouter: Router = Router();
 
 songRouter.get("/", getSongs);
-songRouter.use(
-  fileUpload({
-    limits: { fileSize: config.MAX_FILE_SIZE },
-    abortOnLimit: true,
-    responseOnLimit: "File is too big. Max size is 50MB.",
-  })
-);
+songRouter.use(fileUpload({ debug: true }));
 songRouter.post("/", postSong);
 
 export default songRouter;
