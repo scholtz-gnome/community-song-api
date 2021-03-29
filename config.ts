327206db5f714d0f6a6104d89ca8e0df59f73fdb
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-03-29 13:47:20 +0200

    Add aws access keys env variables

--- config.ts
+++ config.ts
@@ -5,6 +5,7 @@ interface Config {
   NODE_ENV: string;
   MAX_FILE_SIZE: number;
   PROJECT_DIR: string;
+  aws: { accessKey: string | undefined; secretAccessKey: string | undefined };
 }
 
 export const config: Config = {
@@ -12,4 +13,8 @@ export const config: Config = {
   NODE_ENV: process.env.NODE_ENV || "test",
   MAX_FILE_SIZE: 50 * 1024 * 1024,
   PROJECT_DIR: __dirname,
+  aws: {
+    accessKey: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 };
