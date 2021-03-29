0ba4385fb4e709cc714ded88dc517f0084870b37
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-03-29 13:49:09 +0200

    Spike: set up getSongs and postSong with S3

--- src/controllers/songController.ts
+++ src/controllers/songController.ts
@@ -1,12 +1,30 @@
 import { Request, Response } from "express";
 import { UploadedFile } from "express-fileupload";
-import { config } from "../../config";
+import aws from "aws-sdk";
 
-export const getSongs = (req: Request, res: Response) => {
-  // query db for all songs
+export const getSongs = async (req: Request, res: Response) => {
+  try {
+    const s3 = new aws.S3();
+
+    s3.listObjects(
+      {
+        Bucket: "community-song-pdfs",
+      },
+      (err, data) => {
+        try {
+          res.status(200).json({ data });
+        } catch (error) {
+          console.log("Error from S3: " + err);
+          console.log("Error from server: " + error);
+        }
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-export const postSong = (req: Request, res: Response) => {
+export const postSong = async (req: Request, res: Response) => {
   if (req.files === undefined) {
     return res
       .status(400)
@@ -15,19 +33,41 @@ export const postSong = (req: Request, res: Response) => {
 
   const song = req.files.file as UploadedFile;
 
-  song.mv(`${config.PROJECT_DIR}${song.name}`, (err) => {
-    if (err) {
-      return res.status(400).json({
-        success: false,
-        message: err,
-      });
-    }
+  try {
+    const s3 = new aws.S3();
 
-    res.status(200).json({
-      success: true,
-      message: `${song.name} was successfully uploaded`,
-      fileName: song.name,
-      filePath: `${config.PROJECT_DIR}${song.name}`,
-    });
-  });
+    s3.upload(
+      {
+        Bucket: "community-song-pdfs",
+        Key: song.name,
+        Body: song.data,
+      },
+      (err, data) => {
+        try {
+          res.status(200).json({ data });
+        } catch (error) {
+          console.log("Error from S3: " + err);
+          console.log("Error from server: " + error);
+        }
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
+
+  // song.mv(`${config.PROJECT_DIR}${song.name}`, (err) => {
+  //   if (err) {
+  //     return res.status(400).json({
+  //       success: false,
+  //       message: err,
+  //     });
+  //   }
+
+  //   res.status(200).json({
+  //     success: true,
+  //     message: `${song.name} was successfully uploaded`,
+  //     fileName: song.name,
+  //     filePath: `${config.PROJECT_DIR}${song.name}`,
+  //   });
+  // });
 };
