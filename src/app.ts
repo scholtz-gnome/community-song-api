8219483a3165edc3bc05871cc2d540bb0e4bfa57
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-04-06 14:04:44 +0200

    Add wildcard to end of cors origin url

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ export function newApp(): Express {
 
   app.use(
     cors({
-      origin: "https://community-song.herokuapp.com/",
+      origin: "https://community-song.herokuapp.com/*",
     })
   );
   app.use(express.json());
