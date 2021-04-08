1f93c42f7988138118558a903aad59807b1f170f
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-04-08 15:24:41 +0200

    Testing request

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,14 +2,22 @@ import { Request, Response } from "express";
 import config from "../../config";
 
 export const getGoogleRedirect = (req: Request, res: Response) => {
+  // @ts-ignore
+  console.log(req._passport);
+  console.log(req.user);
+
   res.redirect(`${config.APP_URL_ROOT}`);
 };
 
 export const getUserDetails = (req: Request, res: Response) => {
+  // @ts-ignore
+  console.log(req._passport);
+  console.log(req.user);
   res.status(200).json(req.user);
 };
 
 export const getLogout = (req: Request, res: Response) => {
+  console.log(req);
   req.logout();
   res.redirect(`${config.APP_URL_ROOT}`);
 };
