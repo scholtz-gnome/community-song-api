50f4b4eb4fe59d36869ed46d7b5ce7217d643431
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-04-12 11:51:23 +0200

    Set cookie creation to right before redirect

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -12,11 +12,16 @@ const createToken = (id: number) => {
 export const getGoogleRedirect = (req: Request, res: Response) => {
   const user: any | undefined = req.user;
   const token = createToken(user.id);
-  res.cookie("jwt", token, {
-    maxAge: maxAge * 1000,
-    sameSite: false,
-  });
-  res.redirect(`${config.APP_URL_ROOT}`);
+  // res.cookie("jwt", token, {
+  //   httpOnly: true,
+  //   maxAge: maxAge * 1000,
+  // });
+  res
+    .cookie("jwt", token, {
+      httpOnly: true,
+      maxAge: maxAge * 1000,
+    })
+    .redirect(`${config.APP_URL_ROOT}`);
 };
 
 export const getUserDetails = (req: Request, res: Response) => {
