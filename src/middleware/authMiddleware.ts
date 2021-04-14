0fbb88bb4a21b4406dd21d0b4db79c31e1d5d51d
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-04-14 13:47:57 +0200

    Sign token in middleware

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -16,12 +16,34 @@ export const checkUser = (req: Request, res: Response, next: NextFunction) => {
         } else {
           let [user] = await db("user").select().where("id", decodedToken.id);
           req.user = user;
+          const maxAge = 24 * 60 * 60;
+          const id: number = user.id;
+          jwt.sign(
+            { id },
+            config.JWT_SECRET || "",
+            {
+              expiresIn: maxAge,
+            },
+            (err, token) => {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log("getGoogleRedirect token => ", token);
+                res
+                  .cookie("jwt", token, {
+                    httpOnly: true,
+                    maxAge: maxAge * 1000,
+                  })
+                  .redirect(`${config.APP_URL_ROOT}`);
+              }
+            }
+          );
           next();
         }
       }
     );
   } else {
-    res.locals.user = null;
+    console.log("No token");
     next();
   }
 };
