82a57eed17ebf2f1a49f1ba961ef4c1a62fec3ae
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-04-15 14:25:26 +0200

    Implement song delete

--- src/routes/songRouter.ts
+++ src/routes/songRouter.ts
@@ -1,12 +1,20 @@
 import { Router } from "express";
 import fileUpload from "express-fileupload";
-import { getSongs, getSong, postSong } from "../controllers/songController";
+import {
+  getSongs,
+  getSong,
+  getProfileSongs,
+  postSong,
+  deleteSong,
+} from "../controllers/songController";
 
 const songRouter: Router = Router();
 
 songRouter.get("/", getSongs);
 songRouter.get("/:id", getSong);
+songRouter.get("/profileSongs/:email", getProfileSongs);
 songRouter.use(fileUpload());
 songRouter.post("/", postSong);
+songRouter.delete("/:id", deleteSong);
 
 export default songRouter;
