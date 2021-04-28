5f9fc9cc29c74e3756916c90ff4678601de45296
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-04-28 15:46:25 +0200

    Move auth middleware to authRouter

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,8 @@
 import config from "../config";
 import express, { Express } from "express";
 import cors from "cors";
-import passport from "passport";
 import songRouter from "./routes/songRouter";
 import authRouter from "./routes/authRouter";
-import { checkUser } from "./middleware/authMiddleware";
-import cookieParser from "cookie-parser";
 import compression from "compression";
 
 export function newApp(): Express {
@@ -20,10 +17,6 @@ export function newApp(): Express {
     })
   );
   app.use(express.urlencoded({ extended: true }));
-  app.use(cookieParser());
-  app.use(checkUser);
-  app.use(passport.initialize());
-  app.use(passport.session());
 
   app.use("/songs", songRouter);
   app.use("/auth", authRouter);
