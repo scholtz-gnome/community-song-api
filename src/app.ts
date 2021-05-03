835e205ba78577f608bf8ecc328f425206f8cbe3
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-05-03 15:25:58 +0200

    Use csrf middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import config from "../config";
-import express, { Express } from "express";
+import express, { Express, Request, Response, NextFunction } from "express";
 import cors from "cors";
 import songRouter from "./routes/songRouter";
 import authRouter from "./routes/authRouter";
@@ -7,6 +7,7 @@ import compression from "compression";
 import { checkUser } from "./middleware/authMiddleware";
 import cookieParser from "cookie-parser";
 import passport from "passport";
+import csurf from "csurf";
 
 export function newApp(): Express {
   const app = express();
@@ -21,6 +22,19 @@ export function newApp(): Express {
   );
   app.use(express.urlencoded({ extended: true }));
   app.use(cookieParser());
+  app.use(
+    csurf({
+      cookie: {
+        sameSite: "strict",
+        secure: true,
+        httpOnly: true,
+      },
+    }),
+    (req: Request, res: Response, next: NextFunction) => {
+      res.cookie("CSRF-TOKEN", req.csrfToken());
+      next();
+    }
+  );
   app.use(checkUser);
   app.use(passport.initialize());
   app.use(passport.session());
