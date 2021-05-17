fe7d04a9b79b6f1f82285a0e272fc009da21e08e
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-05-17 15:43:22 +0200

    Remove link table and add suggestions

--- db/migrations/20210517100921_create_community_table.ts
+++ db/migrations/20210517100921_create_community_table.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable("community", (table) => {
     table.increments().primary();
     table.string("name").notNullable();
-    table.string("about").defaultTo(null);
+    table.text("about").defaultTo(null);
     table.string("type").notNullable();
     table.timestamp("created_at").defaultTo(knex.fn.now());
     table.timestamp("updated_at").defaultTo(knex.fn.now());
--- db/migrations/20210517102049_create_user_community_table.ts
+++ db/migrations/20210517102049_create_user_community_table.ts
@@ -3,12 +3,18 @@ import { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable("user_community", (table) => {
     table.increments().primary();
-    table.integer("user_id").references("id").inTable("user").notNullable();
+    table
+      .integer("user_id")
+      .references("id")
+      .inTable("user")
+      .notNullable()
+      .index();
     table
       .integer("community_id")
       .references("id")
       .inTable("community")
-      .notNullable();
+      .notNullable()
+      .index();
     table.timestamp("created_at").defaultTo(knex.fn.now());
     table.timestamp("updated_at").defaultTo(knex.fn.now());
   });
