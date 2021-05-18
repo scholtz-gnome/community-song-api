d05d2594fdd8ef662e89493f695bc579d11791a6
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-05-18 20:39:37 +0200

    Add two songsFilesController functions, one as a mock controller

---
+++ src/controllers/songsFilesController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import * as SongFileService from "../services/songFileService";
+
+export const getSongFiles = async (req: Request, res: Response) => {
+  const songId = Number(req.params.id);
+  try {
+    const fileBodies = await SongFileService.fetchSongFiles(songId);
+
+    return res.status(200).json({
+      success: true,
+      message: `File bodies retrieved`,
+      files: fileBodies,
+    });
+  } catch (err) {
+    console.log(err);
+    throw new Error("getSongFiles error");
+  }
+};
+
+export const deleteSongFiles = async (req: Request, res: Response) => {
+  // mock controller function for now
+  console.log(req.params.id);
+  res.status(200).json({
+    success: true,
+    message: "You've made a DELETE request to the /songs/:id/files endpoint",
+  });
+};
