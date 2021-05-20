b23eb9c449e2e1fb97ea06d2fb761ae4d1cd5c9a
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-05-20 14:28:48 +0200

    Remove file upload middleware

--- src/routes/songsRouter.ts
+++ src/routes/songsRouter.ts
@@ -1,11 +1,9 @@
 import express, { Router } from "express";
-import fileUpload from "express-fileupload";
 import * as SongsController from "../controllers/songsController";
 
 const songsRouter: Router = Router();
 
 songsRouter.use(express.json());
-songsRouter.use(fileUpload());
 
 songsRouter.get("/songs", SongsController.getSongs);
 songsRouter.get("songs/:id", SongsController.getSong);
