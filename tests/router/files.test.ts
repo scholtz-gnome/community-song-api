e455b9b9cc501445549badec8d663486a9b7cde7
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-05-20 14:33:08 +0200

    Reorder POST before GET and check file body in GET

--- tests/router/files.test.ts
+++ tests/router/files.test.ts
@@ -8,40 +8,28 @@ describe("filesRouter", () => {
   const filesPath: string = "/files";
   const app: Express = express();
 
-  app.use(filesRouter);
   app.use(songsRouter);
+  app.use(filesRouter);
 
   beforeAll(async () => {
     jest.setTimeout(25000);
+    await db.migrate.rollback();
     await db.migrate.latest();
     await request(app)
       .post("/songs")
-      .field({ title: "Nocturne in Eb" })
-      .field({ artist: "Frédéric Chopin" });
+      .send({ title: "Nocturne in Eb" })
+      .send({ artist: "Frédéric Chopin" });
   });
 
   afterAll(async () => {
-    await db.migrate.rollback();
     await db.destroy();
-  });
-
-  describe("GET", () => {
-    describe("When one song is retrieved from the S3 bucket", () => {
-      it("responds with status code: 200", async () => {
-        const res = await request(app).get(`${filesPath}/1`);
-
-        expect(JSON.parse(res.text).message).toBe("File retrieved");
-        expect(JSON.parse(res.text).success).toBe(true);
-        expect(res.status).toBe(200);
-      });
-    });
   });
 
   describe("POST", () => {
     describe("When one file is uploaded", () => {
       it("responds with status code: 200, message: 'File Nocturne in Eb.pdf created'", async () => {
         const res = await request(app)
-          .post(`${filesPath}`)
+          .post(filesPath)
           .field({ songId: 1 })
           .attach(
             "file",
@@ -51,6 +39,21 @@ describe("filesRouter", () => {
 
         expect(JSON.parse(res.text).message).toBe(
           "File Nocturne in Eb.pdf created"
+        );
+        expect(res.status).toBe(200);
+      });
+    });
+  });
+
+  describe("GET", () => {
+    describe("When one song is retrieved from the S3 bucket", () => {
+      it("responds with status code: 200", async () => {
+        const res = await request(app).get(`${filesPath}/1`);
+
+        expect(JSON.parse(res.text).success).toBe(true);
+        expect(JSON.parse(res.text).message).toBe("File retrieved");
+        expect(JSON.parse(res.text).fileBody).toMatch(
+          new RegExp(/(?=^(.{4})*$)[A-Za-z+0-9/]/g)
         );
         expect(res.status).toBe(200);
       });
