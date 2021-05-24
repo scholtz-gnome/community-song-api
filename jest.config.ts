3e830ebb1d113b5cdb4ac6c9d007121a9d34cc1f
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-05-24 17:14:58 +0200

    Reviewed and test coverage added

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,8 @@
-module.exports = {
+export default {
   testEnvironment: "node",
   preset: "ts-jest",
   testRegex: "(/dist/tests/.*|(\\.|/)(test))\\.[t]s?$",
   verbose: true,
+  collectCoverage: true,
+  coverageDirectory: __dirname + "/tests/coverage",
 };
